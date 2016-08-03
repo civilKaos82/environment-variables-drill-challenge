0ed5080241dae80527a756a87566e5b080da7344
Torey Hickman <torey@toreyhickman.com>
2016-08-03 09:40:31 -0400

    Displays a recap of user data

--- runner.rb
+++ runner.rb
@@ -1,3 +1,15 @@
 require 'octokit'
 
-puts !!Octokit
+def user_recap(user)
+  <<-TEXT.gsub(/^\s+/, "")
+    Your GitHub username is #{user.login}.
+    You have #{user.followers} followers on GitHub.
+    You are following #{user.following} users.
+  TEXT
+end
+
+
+client = Octokit::Client.new(:access_token => ENV["GITHUB_ACCESS_TOKEN"])
+
+user = client.user
+puts user_recap(user)
