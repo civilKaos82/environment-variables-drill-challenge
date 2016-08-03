5f1dea09d0bac80877edae3867fcea695aeb53b2
Torey Hickman <torey@toreyhickman.com>
2016-08-03 09:48:27 -0400

    Adds comments to explain code

--- runner.rb
+++ runner.rb
@@ -9,7 +9,11 @@ def user_recap(user)
 end
 
 
+# Create the client object which will make requests to the GitHub API,
+# using an access token to authenticate as a specific user.
 client = Octokit::Client.new(:access_token => ENV["GITHUB_ACCESS_TOKEN"])
 
+
+# Use the client to request data for the authenticated user.
 user = client.user
 puts user_recap(user)
